723d0264b22fda9d6c1072a9d6e4f48cc2a26c31
Kashif Hussain <57903864+Hkashif722@users.noreply.github.com>
2026-02-17 17:10:53 +0530

    Add APIConst struct with API constants

---
+++ Sources/SwiftUIUtilities/Core/APIConst.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+internal struct APIConst {
+    
+    static nonisolated(unsafe) var baseURL = ""
+    static nonisolated(unsafe) var lxpOPath = ""
+    static nonisolated(unsafe) var lxpBlobPath = ""
+    static nonisolated(unsafe) var lxpBlobPath1 = ""
+    static let courseBaseUrl = "/api";
+    static let versionAPI = "v1";
+    static let lxpPath = "/org-content"
+    static let ContentPath = "https://content.gogetempowered.com"
+}
+
